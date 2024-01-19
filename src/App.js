22c50090c71e7f2e85c7fb796e5107a4b11e0135
Ricardo Lafontant <jrl9298@gmail.com>
2024-01-19 02:17:28 -0500

    Dashboard route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavigationBar from "./components/NavBar";
 import Home from "./pages/Home"; // Import your other components/pages.
 import Footer from "./components/Footer";
 import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
       <Footer />
     </Router>
