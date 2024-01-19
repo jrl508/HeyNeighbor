91478d1926532a1cb39cfd951d64bfd16683fa23
Ricardo Lafontant <jrl9298@gmail.com>
2024-01-19 02:23:03 -0500

    WIP-set up dashboard

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,137 @@
+import React from "react";
+import styles from "../styles/Dashboard.module.css";
+import ProfilePH from "../images/profile_ph.svg";
+
+/*
+
+Dashboard Components:
+
+    1. User Profile Overview:
+        Display user details like name, profile picture, and contact information.
+        Overview of the user's rental history, both as an owner and a renter.
+
+    2.Current Listings: 
+        Tools currently available for rent by the user.
+        Include details like tool name, description, rental status, and rental price.
+    
+    3. Renting History:
+        A section displaying tools the user has rented in the past.
+        Include details such as the rental period, cost, and feedback from the owner.
+
+    4. Owned Tools:
+        A list of tools the user owns and has listed for rent.
+        Include details like tool name, description, availability status, and rental income.
+
+    5. Transaction History:
+        A log of financial transactions related to tool rentals.
+        Details should include dates, amounts, and the status of the transaction.
+    
+    6. Messages and Notifications:
+        A messaging system for communication between users.
+        Notifications for new rental requests, messages, and important updates.
+    
+    7. Rental Requests:
+        A section displaying pending and accepted rental requests.
+        Include details like tool name, requester's details, and rental dates.
+    
+    8. Settings and Preferences:
+        Allow users to manage account settings.
+        Preferences for notifications, privacy settings, and payment methods.
+    
+    9. Reviews and Ratings:
+        A place to view and leave reviews and ratings for both tool owners and renters.
+        Encourages trust and transparency within the community.
+    
+    10. Help and Support:
+
+Access to a help center or FAQ section.
+Contact options for customer support.
+    11. Verification and Security:
+
+Ensure the user's account is verified and secure.
+Options for two-factor authentication and password management.
+    12. Search and Filters:
+
+Tools to search for specific tools or filter based on categories, location, or other relevant criteria.
+
+*/
+
+const Dashboard = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div
+        className={styles.left}
+        style={{
+          borderRight: "solid gray 1px",
+        }}
+      >
+        <img
+          src={ProfilePH}
+          alt="profile pic"
+          style={{ height: "150px", width: "150px" }}
+        />
+        <h3>John Doe</h3>
+        <span>4.8/5 stars</span>
+        <h5>Taunton, MA</h5>
+      </div>
+      <div className={styles.center}>
+        <div
+          style={{
+            width: "100%",
+          }}
+        >
+          <h3
+            style={{
+              alignSelf: "flex-start",
+              margin: "0 0 1em",
+            }}
+          >
+            Home
+          </h3>
+          <div className={styles.card}>
+            <ul>
+              <li
+                style={{
+                  borderBottom: "1px solid lightgray",
+                }}
+              >
+                Item 1
+              </li>
+              <li
+                style={{
+                  borderBottom: "1px solid lightgray",
+                }}
+              >
+                Item 2
+              </li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className={styles.right}>
+        <h3
+          style={{
+            margin: "0 0 1em",
+          }}
+        >
+          Notification Center
+        </h3>
+        <div className={styles.card}>
+          <ul>
+            <li
+              style={{
+                borderBottom: "1px solid lightgray",
+              }}
+            >
+              Item 1
+            </li>
+            <li>Item 2</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
