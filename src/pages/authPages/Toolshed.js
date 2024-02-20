c408af4197f91a511048b066913e35ddaba0988d
Ricardo Lafontant <jrl9298@gmail.com>
2024-02-20 14:54:36 -0500

    modal component to view specific tool

--- src/pages/authPages/Toolshed.js
+++ src/pages/authPages/Toolshed.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import toolData from "../../seed/toolData";
 
 const Toolshed = () => {
-  const available = toolData.filter((tool) => tool.available);
+  const [open, setOpen] = useState(false);
+  const [selectedTool, setSelectedTool] = useState(null);
+
+  const handleClick = (t) => {
+    setSelectedTool(t);
+    setOpen(true);
+  };
 
   return (
-    <div>
+    <div className="is-clipped">
       <div className="title is-5">Toolshed</div>
       <div
         className="tool-list"
@@ -15,16 +21,16 @@ const Toolshed = () => {
           gap: "25px",
         }}
       >
-        {toolData.map((tool, index) => (
+        {toolData.map((tool) => (
           <div
-            key={index}
+            key={tool.id}
             className="card"
             style={{
               maxWidth: "280px",
               minWidth: "200px",
             }}
           >
-            <div className="card-image">
+            <div className="card-image" onClick={() => handleClick(tool)}>
               <figure className="image">
                 <img
                   src="https://bulma.io/images/placeholders/256x256.png"
@@ -47,6 +53,51 @@ const Toolshed = () => {
             </div>
           </div>
         ))}
+      </div>
+      <div className={`modal ${open && "is-active"}`}>
+        <div className="modal-background">
+          <div
+            className="modal-card"
+            style={{
+              marginTop: "5%",
+            }}
+          >
+            <header className="modal-card-head">
+              <p className="modal-card-title">
+                {selectedTool && selectedTool.toolName}
+              </p>
+              <button
+                className="delete"
+                onClick={() => {
+                  setOpen(false);
+                  setSelectedTool(null);
+                }}
+              ></button>
+            </header>
+            <section className="modal-card-body">
+              <div className="card-image">
+                <figure className="image">
+                  <img
+                    src="https://bulma.io/images/placeholders/480x480.png"
+                    alt="Placeholder image"
+                  />
+                </figure>
+              </div>
+              <div>
+                {selectedTool ? (
+                  <div>
+                    <p>Description: {selectedTool.description}</p>
+                    <p>Rental Rate: {selectedTool.rentalRate}</p>
+                    {!selectedTool.available && <p>Rented By: Some Neighbor</p>}
+                  </div>
+                ) : null}
+              </div>
+            </section>
+            <footer className="modal-card-foot">
+              <button className="button is-info">Edit Info</button>
+            </footer>
+          </div>
+        </div>
       </div>
     </div>
   );
