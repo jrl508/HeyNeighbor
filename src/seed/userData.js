f2cf83c21a6085220010d0e045aadb4995be1833
Ricardo Lafontant <jrl9298@gmail.com>
2024-04-04 17:52:06 -0400

    update seed data

---
+++ src/seed/userData.js
@@ -0,0 +1,42 @@
+export const usersData = [
+  {
+    id: 1,
+    name: "John Doe",
+    zipcode: "02780",
+  },
+  {
+    id: 2,
+    name: "Jane Smith",
+    zipcode: "02780",
+  },
+  {
+    id: 3,
+    name: "Alice Johnson",
+    zipcode: "02780",
+  },
+  {
+    id: 4,
+    name: "Bob Brown",
+    zipcode: "02780",
+  },
+  {
+    id: 5,
+    name: "Emily Davis",
+    zipcode: "02780",
+  },
+  {
+    id: 6,
+    name: "Michael Wilson",
+    zipcode: "02301",
+  },
+  {
+    id: 7,
+    name: "Sarah Lee",
+    zipcode: "02301",
+  },
+  {
+    id: 8,
+    name: "David Martinez",
+    zipcode: "02301",
+  },
+];
