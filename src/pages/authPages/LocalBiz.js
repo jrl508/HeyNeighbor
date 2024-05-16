39c8e08f1559acbfc5f6d7f4a67fd6672dcf1a2c
Ricardo Lafontant <jrl9298@gmail.com>
2024-05-16 01:35:07 -0400

    WIP - update form

--- src/pages/authPages/LocalBiz.js
+++ src/pages/authPages/LocalBiz.js
@@ -3,47 +3,366 @@ import Icon from "@mdi/react";
 import {
   mdiEmail,
   mdiFacebook,
-  mdiLink,
   mdiLinkVariant,
   mdiPhone,
   mdiWeb,
 } from "@mdi/js";
 
-const BusinessForm = () => {
+const BusinessForm = ({ setOpenBizForm }) => {
+  const [name, setName] = useState("");
+  const [type, setType] = useState("");
+  const [address, setAddress] = useState("");
+  const [phone, setPhone] = useState("");
+  const [startTime, setStartTime] = useState({
+    hour: "12",
+    minute: "00",
+    mer: "AM",
+  });
+  const [endTime, setEndTime] = useState({
+    hour: "12",
+    minute: "00",
+    mer: "AM",
+  });
+  const [days, setDays] = useState({
+    mon: false,
+    tue: false,
+    wed: false,
+    thu: false,
+    fri: false,
+    sat: false,
+    sun: false,
+  });
+
+  const handleStartTimeChange = (event, field) => {
+    const payload = { ...startTime };
+    payload[field] = event.target.value;
+    setStartTime(payload);
+  };
+
+  const handleEndTimeChange = (event, field) => {
+    const payload = { ...endTime };
+    payload[field] = event.target.value;
+    setEndTime(payload);
+  };
+
+  const handleCheckDay = (event) => {
+    const payload = { ...days };
+    payload[event.target.name] = !days[event.target.name];
+    setDays(payload);
+  };
+  const hours = Array.from({ length: 12 }, (_, i) => {
+    const hour = (i + 1).toString().padStart(2, "0");
+    return { value: hour, label: hour };
+  });
+
+  const minutes = Array.from({ length: 60 }, (_, i) => {
+    const minute = i.toString().padStart(2, "0");
+    return { value: minute, label: minute };
+  });
+
+  const handleSubmit = () => {
+    const payload = {
+      name,
+      type,
+      address,
+      phone,
+      schedule: {
+        startTime,
+        endTime,
+        days,
+      },
+    };
+    console.log("Submitting", payload);
+    setOpenBizForm(false);
+  };
+
   return (
-    <div className="business-form">
-      <div className="field">
-        <label className="label">Business Name</label>
-        <div className="control">
-          <input className="input" type="text" />
+    <>
+      <div className="business-form modal-card-body">
+        <div className="field">
+          <label className="label">Business Name</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="Business Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Business Type</label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder='Ex. "General Contractor"'
-          />
+        <div className="field">
+          <label className="label">Business Type</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              placeholder='Ex. "General Contractor"'
+              name="Business Type"
+              value={type}
+              onChange={(e) => setType(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Location</label>
-        <div className="control">
-          <input className="input" type="text" />
+        <div className="field">
+          <label className="label">Location</label>
+          <div className="control">
+            <input
+              className="input"
+              type="text"
+              name="Address"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="field">
-        <label className="label">Phone Number</label>
-        <div className="control">
-          <input className="input" type="text" />
+        <div className="field">
+          <label className="label">Phone Number</label>
+          <div className="control">
+            <input
+              className="input"
+              type="tel"
+              name="Phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Business Hours</label>
+          <div
+            className="biz-hours-container"
+            style={{ display: "flex", columnGap: "25px", flexFlow: "row wrap" }}
+          >
+            <div
+              className="start-time-group"
+              style={{ display: "flex", columnGap: "5px" }}
+            >
+              <span>
+                <b>Start: </b>
+              </span>
+              <select
+                id="start-time"
+                value={startTime.hour}
+                onChange={(e) => handleStartTimeChange(e, "hour")}
+              >
+                {hours.map((h) => (
+                  <option key={h.value} value={h.value}>
+                    {h.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                value={startTime.minute}
+                onChange={(e) => handleStartTimeChange(e, "minute")}
+              >
+                {minutes.map((m) => (
+                  <option key={m.value} value={m.value}>
+                    {m.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                value={startTime.mer}
+                onChange={(e) => handleStartTimeChange(e, "mer")}
+              >
+                <option value="AM">AM</option>
+                <option value="PM">PM</option>
+              </select>
+            </div>
+            <div
+              className="end-time-group"
+              style={{ display: "flex", columnGap: "5px" }}
+            >
+              <span>
+                <b>End: </b>
+              </span>
+              <select
+                id="end-time"
+                value={endTime.hour}
+                onChange={(e) => handleEndTimeChange(e, "hour")}
+              >
+                {hours.map((h) => (
+                  <option key={h.value} value={h.value}>
+                    {h.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                value={endTime.minute}
+                onChange={(e) => handleEndTimeChange(e, "minute")}
+              >
+                {minutes.map((m) => (
+                  <option key={m.value} value={m.value}>
+                    {m.label}
+                  </option>
+                ))}
+              </select>
+              <select
+                value={endTime.mer}
+                onChange={(e) => handleEndTimeChange(e, "mer")}
+              >
+                <option value="AM">AM</option>
+                <option value="PM">PM</option>
+              </select>
+            </div>
+            <div
+              className="days-container"
+              style={{ flex: "1 100%", marginTop: "10px" }}
+            >
+              <fieldset>
+                <div
+                  style={{
+                    display: "flex",
+                    justifyContent: "start",
+                    columnGap: "10px",
+                  }}
+                >
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Mon</label>
+                    <input
+                      type="checkbox"
+                      id="monday"
+                      name="mon"
+                      checked={days.mon}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Tue</label>
+                    <input
+                      type="checkbox"
+                      id="tuesday"
+                      name="tue"
+                      checked={days.tue}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Wed</label>
+                    <input
+                      type="checkbox"
+                      id="wednesday"
+                      name="wed"
+                      checked={days.wed}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Thu</label>
+                    <input
+                      type="checkbox"
+                      id="thursday"
+                      name="thu"
+                      checked={days.thu}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Fri</label>
+                    <input
+                      type="checkbox"
+                      id="friday"
+                      name="fri"
+                      checked={days.fri}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Sat</label>
+                    <input
+                      type="checkbox"
+                      id="saturday"
+                      name="sat"
+                      checked={days.sat}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                  <div
+                    style={{
+                      display: "flex",
+                      columnGap: "5px",
+                      alignItems: "center",
+                    }}
+                  >
+                    <label className="has-text-weight-semibold">Sun</label>
+                    <input
+                      type="checkbox"
+                      id="sunday"
+                      name="sun"
+                      checked={days.sun}
+                      onChange={(e) => handleCheckDay(e)}
+                    />
+                  </div>
+                </div>
+              </fieldset>
+            </div>
+          </div>
+        </div>
+        <div
+          className="form-modifiers"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            padding: "0 5px",
+          }}
+        >
+          <span className="is-clickable has-text-info">Add Link</span>
         </div>
       </div>
-    </div>
+      <div className="modal-card-foot">
+        <div className="buttons">
+          <button onClick={handleSubmit} className="button is-info">
+            Submit
+          </button>
+          <button
+            onClick={() => setOpenBizForm(false)}
+            className="button is-danger"
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </>
   );
 };
 
@@ -97,7 +416,7 @@ const LocalBiz = () => {
                 <p className="job-type">General Contractor</p>
                 {/* TODO: Make Clickable to open modal of user reviews */}
                 <p className="ratings">
-                  4.0{" "}
+                  {Number.parseFloat(rating).toFixed(1)}{" "}
                   {[...Array(5)].map((star, index) => {
                     const currentRating = index + 1;
 
@@ -192,25 +511,7 @@ const LocalBiz = () => {
           <header className="modal-card-head">
             <div className="modal-card-title">Recommend a Business</div>
           </header>
-          <div className="modal-card-body">
-            <BusinessForm />
-          </div>
-          <div className="modal-card-foot">
-            <div className="buttons">
-              <button
-                onClick={() => setOpenBizForm(false)}
-                className="button is-info"
-              >
-                Submit
-              </button>
-              <button
-                onClick={() => setOpenBizForm(false)}
-                className="button is-danger"
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
+          <BusinessForm setOpenBizForm={setOpenBizForm} />
         </div>
       </div>
     </div>
