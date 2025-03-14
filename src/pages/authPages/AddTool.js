25da6a42c3e75781c2a64f4ca26a3eead0457a15
Ricardo Lafontant <jrl9298@gmail.com>
2025-03-14 03:30:30 -0400

    WIP added tooltip

--- src/pages/authPages/AddTool.js
+++ src/pages/authPages/AddTool.js
@@ -3,6 +3,7 @@ import { mdiArrowLeft, mdiUpload } from "@mdi/js";
 import React from "react";
 import { Link } from "react-router-dom";
 import Tooltip from "../../components/Tooltip";
+import { mdiInformationOutline } from "@mdi/js";
 
 function AddTool() {
   const toolCategories = [
@@ -37,16 +38,16 @@ function AddTool() {
   const handleDeliveryAvailableChange = (e) =>
     setDeliveryAvailable(e.target.checked);
   return (
-    <div>
-      <div className="new-tool-header is-flex is-flex-direction-row mb-5 is-justify-content-space-between">
-        <div className="title is-5">Add New Tool</div>
-        <Link to="/dashboard/toolshed" className="has-text-black">
+    <div style={{ maxWidth: "1200px", margin: "0 auto" }}>
+      <div className="new-tool-header is-flex is-flex-direction-row mb-5">
+        <Link to="/dashboard/toolshed" className="has-text-black mr-5">
           <div className="icon">
             <Icon path={mdiArrowLeft} size={1} />
           </div>
         </Link>
+        <div className="title is-5">Add New Tool</div>
       </div>
-      <div className="card pl-6 py-6">
+      <div className="card p-6">
         <div className="card-content fixed-grid has-10-cols">
           <div className="grid">
             <div className="field cell is-col-span-5">
@@ -61,7 +62,7 @@ function AddTool() {
                 />
               </div>
             </div>
-            <div className="field cell is-col-start-7">
+            <div className="field cell is-col-start-8 is-col-span-3">
               <label className="label">Tool Category</label>
               <div className="control">
                 <div className="select">
@@ -90,7 +91,7 @@ function AddTool() {
                 />
               </div>
             </div>
-            <div className="field cell is-col-start-7">
+            <div className="field cell is-col-start-8 is-col-span-3">
               <label className="label">Rental Rate</label>
               <div className="control">
                 <input
@@ -124,18 +125,29 @@ function AddTool() {
                 </label>
               </div>
             </div>
-            <div className="field cell is-row-start-3 is-col-start-7 is-col-span-2">
-              <label className="checkbox">
-                <input
-                  type="checkbox"
-                  checked={deliveryAvailable}
-                  onChange={handleDeliveryAvailableChange}
-                />
-                <span className="ml-2 has-text-weight-medium">
-                  Delivery Available
-                  <Tooltip />
-                </span>
-              </label>
+            <div className="field cell is-row-start-3 is-col-start-8 is-col-span-3">
+              <div style={{ display: "flex", alignItems: "center" }}>
+                <label className="checkbox">
+                  <input
+                    type="checkbox"
+                    checked={deliveryAvailable}
+                    onChange={handleDeliveryAvailableChange}
+                  />
+                  <span className="ml-2 has-text-weight-medium">
+                    Delivery Available
+                  </span>
+                </label>
+                <Tooltip
+                  position="bottom"
+                  content="Let others know if you are willing to deliver the tool to them. For an added fee, of course!"
+                >
+                  <Icon
+                    path={mdiInformationOutline}
+                    size={1}
+                    className="ml-2"
+                  />
+                </Tooltip>
+              </div>
             </div>
             <div className="field cell is-row-start-4">
               <div className="control">
